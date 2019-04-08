9af070a247c08c4504a45c1d8e421aa8e1c4e17b
JohnWJackson <39539455+JohnWJackson@users.noreply.github.com>
2019-04-08 01:57:06 -0400

    BrawlSim 1.0
    -- Working version
    -- drawOptimalUnit diagnostic function

--- BrawlSimLib/include/BrawlSim.hpp
+++ BrawlSimLib/include/BrawlSim.hpp
@@ -12,12 +12,25 @@
 namespace BrawlSim
 {
 	/* Returns the optimal UnitType of a FAP simulation using Monte Carlo sim positions
+	** Checks to make sure units/unittypes are valid simulation units.
+	** Doesn't check if the return UnitType is actually buildable.
+	** 
+	** @param  BWAPI::UnitType::set friendly_types - BWAPI's custom set for unittypes. Desirable unique UnitTypes to sim for the "best" out of.
+	** @param  BWAPI::Unitset enemy_units - BWAPI's custom set for units. Enemy units that the friendly UnitType will fight against.
+	** @param  int sim_size - Approximate number of units for each force's army composition in the sim. Default 10.
 	**
-	** @param   BWAPI::UnitType::set friendly_types - BWAPI's custom set for unittypes. Desirable combat UnitTypes to sim for the "best" out of
-	** @param   BWAPI::Unitset enemy_units - BWAPI's custom set for units. Enemy units that the friendly UnitType will fight against
-	** @param   int sim_size - Approximate number of units for each force in the sim. If less than enemy units, makes an army composition for desired size. Default 10.
-	**														
-	** @return  BWAPI::UnitType - "best" UnitType of a FAP Sim
+	** @return BWAPI::UnitType - "best" UnitType out of the FAP Sim.
 	*/
 	BWAPI::UnitType returnOptimalUnit(const BWAPI::UnitType::set& friendly_types, const BWAPI::Unitset& enemy_units, int sim_size = 10);
+
+	/* Draw the 'would-be' optimal unit to the screen at the desired coordinates
+	**
+	** @param  x - X coordinate frame position
+	** @param  y - Y coordinate frame position
+	*/
+	void drawOptimalUnit(const int x, const int y);
+
+
+	// The stored optimal unit
+	static BWAPI::UnitType optimal_unit = BWAPI::UnitTypes::None;
 }
--- BrawlSimLib/src/BrawlSim.cpp
+++ BrawlSimLib/src/BrawlSim.cpp
@@ -1,5 +1,4 @@
 #include "..\..\BrawlSimLib\include\BrawlSim.hpp"
-#include "..\..\BrawlSimLib\include\BrawlSim\UnitData.hpp"
 
 
 namespace BrawlSim
@@ -7,106 +6,16 @@ namespace BrawlSim
 
 	namespace
 	{
-
-		/// Generate a random position for the unit based on the unit speed
-		BWAPI::Position positionMCFAP(const impl::UnitData& ud)
-		{
-			std::default_random_engine generator;  //Will be used to obtain a seed for the random number engine
-
-			double speed = ud.player->topSpeed(ud.type);
-			std::uniform_int_distribution<int> dis(static_cast<int>(-speed) * 4, static_cast<int>(speed) * 4);
-
-			int rand_x = dis(generator);
-			int rand_y = dis(generator);
-
-			return BWAPI::Position(rand_x, rand_y);
-		}
-
-		/// Convert a UnitData to a FAP::Unit
-		auto convertToFAPUnit(impl::UnitData& ud)
-		{
-			int groundDamage(ud.player->damage(ud.type.groundWeapon()));
-			int groundCooldown(ud.type.groundWeapon().damageFactor() && ud.type.maxGroundHits() ? ud.player->weaponDamageCooldown(ud.type) / (ud.type.groundWeapon().damageFactor() * ud.type.maxGroundHits()) : 0);
-			int groundMaxRange(ud.player->weaponMaxRange(ud.type.groundWeapon()));
-			int groundMinRange(ud.type.groundWeapon().minRange());
-			BWAPI::DamageType groundDamageType(ud.type.groundWeapon().damageType());
-
-			int airDamage(ud.player->damage(ud.type.airWeapon()));
-			int airCooldown(ud.type.airWeapon().damageFactor() && ud.type.maxAirHits() ? ud.type.airWeapon().damageCooldown() / (ud.type.airWeapon().damageFactor() * ud.type.maxAirHits()) : 0);
-			int airMaxRange(ud.player->weaponMaxRange(ud.type.airWeapon()));
-			int airMinRange(ud.type.airWeapon().minRange());
-			BWAPI::DamageType airDamageType(ud.type.airWeapon().damageType());
-
-			if (ud.type == BWAPI::UnitTypes::Protoss_Carrier)
-			{
-				groundDamage = ud.player->damage(BWAPI::UnitTypes::Protoss_Interceptor.groundWeapon());
-				groundDamageType = BWAPI::UnitTypes::Protoss_Interceptor.groundWeapon().damageType();
-				groundCooldown = 5;
-				groundMaxRange = 32 * 8;
-
-				airDamage = groundDamage;
-				airDamageType = groundDamageType;
-				airCooldown = groundCooldown;
-				airMaxRange = groundMaxRange;
-			}
-			else if (ud.type == BWAPI::UnitTypes::Protoss_Reaver)
-			{
-				groundDamage = ud.player->damage(BWAPI::WeaponTypes::Scarab);
-			}
-
-			return FAP::makeUnit<impl::UnitData*>()
-
-				.setUnitType(ud.type)
-				.setUnitSize(ud.type.size())
-
-				.setSpeed(static_cast<float>(ud.player->topSpeed(ud.type)))
-				.setPosition(positionMCFAP(ud))
-
-				.setHealth(ud.type.maxHitPoints())
-				.setMaxHealth(ud.type.maxHitPoints())
-
-				.setArmor(ud.player->armor(ud.type))
-				.setShields(ud.type.maxShields())
-				.setMaxShields(ud.type.maxShields())
-				.setShieldUpgrades(ud.type == BWAPI::UnitTypes::Protoss_Zealot ? ud.player->getUpgradeLevel(BWAPI::UpgradeTypes::Protoss_Plasma_Shields) : 0)
-
-				.setGroundDamage(groundDamage)
-				.setGroundCooldown(groundCooldown)
-				.setGroundMaxRange(groundMaxRange)
-				.setGroundMinRange(groundMinRange)
-				.setGroundDamageType(groundDamageType)
-
-				.setAirDamage(airDamage)
-				.setAirCooldown(airCooldown)
-				.setAirMaxRange(airMaxRange)
-				.setAirMinRange(airMinRange)
-				.setAirDamageType(airDamageType)
-
-				.setAttackerCount(ud.type == BWAPI::UnitTypes::Protoss_Carrier ? 5 : 0) //assume carriers have 5 interceptors
-				.setStimmed(ud.type == BWAPI::UnitTypes::Terran_Marine ? ud.player->hasResearched(BWAPI::TechTypes::Stim_Packs) : 0)
-
-				.setElevation(0)
-				.setFlying(ud.type.isFlyer())
-				.setOrganic(ud.type.isOrganic())
-
-				// These are already calculated above but have to be set to something else they throw FAP flags
-				.setArmorUpgrades(NULL)
-				.setAttackUpgrades(NULL)
-				.setShieldUpgrades(NULL)
-				.setSpeedUpgrade(NULL)
-				.setAttackSpeedUpgrade(NULL)
-				.setAttackCooldownRemaining(NULL)
-				.setRangeUpgrade(NULL)
-				;
-		}
-
 		/// Checks if a UnitType is a suitable type for the sim
 		bool isValidType(const BWAPI::UnitType& type)
 		{
-			// No workers, buildings, or interceptors
+			// No workers, buildings, heroes, or unbuildable unittypes
 			if (type.isWorker() ||
 				type.isBuilding() ||
-				type == BWAPI::UnitTypes::Protoss_Interceptor)
+				type.isHero() ||
+				type == BWAPI::UnitTypes::Protoss_Interceptor ||
+				type == BWAPI::UnitTypes::Protoss_Scarab ||
+				type == BWAPI::UnitTypes::Zerg_Infested_Terran)
 			{
 				return false;
 			}
@@ -122,29 +31,23 @@ namespace BrawlSim
 		}
 
 		/// Converts valid friendly UnitTypes to FAP::Units and add to the sim
-		void addFriendlyToFAP(FAP::FastAPproximation<impl::UnitData*>& fap_object, const BWAPI::UnitType::set& unit_types, const int sim_size)
+		void addFriendlyToFAP(FAP::FastAPproximation<impl::UnitData*>& fap_object, std::vector<impl::UnitData>& friendly_ud, const int sim_size)
 		{
-			BWAPI::Player self = BWAPI::Broodwar->self();
-
-			for (const auto& ut : unit_types)
+			for (auto& u : friendly_ud)
 			{
-				if (ut.isTwoUnitsInOneEgg()) //zerglings and scourges
+				if (u.type.isTwoUnitsInOneEgg()) //zerglings and scourges
 				{
-					impl::UnitData ud{ ut, self };
-
 					for (int i = 0; i < sim_size * 2; ++i) // add double the units
 					{
-						fap_object.addUnitPlayer1(convertToFAPUnit(ud));
+						fap_object.addUnitPlayer1(u.convertToFAPUnit());
 					}
 				}
 
-				else if (isValidType(ut))
+				else if (isValidType(u.type))
 				{
-					impl::UnitData ud{ ut, self };
-
 					for (int i = 0; i < sim_size; ++i)
 					{
-						fap_object.addUnitPlayer1(convertToFAPUnit(ud));
+						fap_object.addUnitPlayer1(u.convertToFAPUnit());
 					}
 				}
 			}
@@ -157,7 +60,7 @@ namespace BrawlSim
 
 			int unit_total = 0;
 			std::map<BWAPI::UnitType, int> count;
-			for (auto u : units)
+			for (auto u : units) //count unittypes
 			{
 				BWAPI::UnitType ut = u->getType();
 				if (isValidType(ut))
@@ -172,13 +75,13 @@ namespace BrawlSim
 				for (const auto& ut : count)
 				{
 					impl::UnitData ud{ ut.first, enemy };
-					fap_object.addUnitPlayer2(convertToFAPUnit(ud));
+					fap_object.addUnitPlayer2(ud.convertToFAPUnit());
 				}
 			}
 			else  //scale the army composition before adding
 			{
-				int actual_sim_size = 0; //Original sim_size not guarenteed
-				for (const auto& ut : count)
+				int actual_sim_size = 0; //Original sim_size not guaranteed
+				for (const auto& ut : count) 
 				{
 					double percent = ut.second / unit_total;
 					int scaled_count = static_cast<int>(std::round(percent * sim_size));
@@ -187,54 +90,24 @@ namespace BrawlSim
 					impl::UnitData ud{ ut.first, enemy };
 					for (int i = 0; i < scaled_count; ++i)
 					{
-						fap_object.addUnitPlayer2(convertToFAPUnit(ud));
+						fap_object.addUnitPlayer2(ud.convertToFAPUnit());
 					}
 				}
 				sim_size = actual_sim_size; //set the sim size to match the enemy's size
 			}
 		}
 
-		/// Get the Total FAP Score of units for the player
-		/*int getTotalFAPScore(FAP::FastAPproximation<impl::UnitData*>& fap)
+		/// Updates the score map to post FAP sim values
+		void updateScores(std::vector<FAP::FAPUnit<impl::UnitData*>>& fap_vector, std::map<BWAPI::UnitType, int>& scores)
 		{
-			auto addScores = [&](int currentScore, auto FAPunit)
-			{
-				return static_cast<int>(currentScore + FAPunit.data->pre_sim_score_);
-			};
-
-			if (friendly && fap.getState().first && !fap.getState().first->empty())
-			{
-				return std::accumulate(fap.getState().first->begin(), 
-									   fap.getState().first->end(), 
-					                   0, 
-									   addScores);
-			}
-			else if (!friendly && fap.getState().second && !fap.getState().second->empty())
-			{
-				return std::accumulate(fap.getState().second->begin(),
-									   fap.getState().second->end(),
-									   0,
-									   addScores);
-			}
-			else return 0;
-		}*/
-
-		/// Updates the FAP Score of players units after sim
-		std::map<BWAPI::UnitType, int> getFAPscores(std::vector<FAP::FAPUnit<impl::UnitData*>>& fap_vector)
-		{
-			std::map<BWAPI::UnitType, int> scores;
 			for (auto& fu : fap_vector)
 			{
-				if (fu.data)
-				{
-					double proportion_health = (fu.health + fu.shields) / static_cast<double>(fu.maxHealth + fu.maxShields);
-					scores[fu.data->type] = static_cast<int>(fu.data->pre_sim_score * proportion_health);
-				}
+				double proportion_health = (fu.health + fu.shields) / static_cast<double>(fu.maxHealth + fu.maxShields);
+				scores[fu.unitType] = static_cast<int>(fu.data->pre_sim_score * proportion_health);
 			}
-
-			return scores;
 		}
 
+		/// Return the UnitType with the highest score. If scores are tied, chooses more "flexible" UnitType.
 		BWAPI::UnitType getBestScoredType(const std::map<BWAPI::UnitType, int>& scores)
 		{
 			int best_sim_score = INT_MIN;
@@ -269,24 +142,53 @@ namespace BrawlSim
 
 	}
 
-	///Simply returns the unittype that is the "best" of a BuildFAP sim.
+	/// Returns the unittype that is the "best" of a BuildFAP sim.
 	BWAPI::UnitType returnOptimalUnit(const BWAPI::UnitType::set& friendly_types, const BWAPI::Unitset& enemy_units, int sim_size)
 	{	
+		// Returns BWAPI::UnitType::None
+		if (friendly_types.empty())
+		{
+			return optimal_unit;
+		}
+
+		std::map<BWAPI::UnitType, int> scores;
+		std::vector<impl::UnitData> friendly_ud;
+
+		// Convert each UnitType to UnitData and get initial score
+		for (const auto& ut : friendly_types)
+		{
+			impl::UnitData ud{ ut, BWAPI::Broodwar->self() };
+
+			friendly_ud.push_back(ud);
+
+			scores[ut] = ud.pre_sim_score;
+		}
+
+		// Return best initial score
+		if (enemy_units.empty())
+		{
+			optimal_unit = getBestScoredType(scores);
+			return optimal_unit;
+		}
+
 		FAP::FastAPproximation<impl::UnitData*> MCfap;
 
 		addEnemyToFAP(MCfap, enemy_units, sim_size); // Add Enemy first - changes the sim_size
-		addFriendlyToFAP(MCfap, friendly_types, sim_size);
+		addFriendlyToFAP(MCfap, friendly_ud, sim_size);
 
 		MCfap.simulate(); //default 96 frames
-		
-		//int friendly_fap_score = getTotalFAPScore<true>(MCfap);
-		//int enemy_fap_score = getTotalFAPScore<false>(MCfap);
 
-		std::map<BWAPI::UnitType, int> scores{ getFAPscores(*MCfap.getState().first) };
+		updateScores(*MCfap.getState().first, scores);
 
-		BWAPI::UnitType res{ getBestScoredType(scores) };
-		
-		return res;
+		optimal_unit = getBestScoredType(scores);
+
+		return optimal_unit;
+	}
+
+	/// Draw the optimal unit to the desired coordinates
+	void drawOptimalUnit(const int x, const int y)
+	{
+		BWAPI::Broodwar->drawTextScreen(x, y, "Brawl Unit: %s", optimal_unit.c_str());
 	}
 
 }
